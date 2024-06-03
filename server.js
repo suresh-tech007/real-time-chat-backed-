155c320940e5bbecd768c3896f6922ff21b60276
suresh-tech007 <sk4896323@gmail.com>
2024-06-03 17:43:37 +0530

    ad nodemon

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ import dotenv from  "dotenv"
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
-import path from "path"
+import cors from "cors"
+app.use(cors({
+    origin: 'https://real-time-chat-whatsapp.netlify.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true  
+}));
 
+
+app.options('*', cors());
 
 import connectToMongoDB from "./db/connectToMongooseDB.js"
 import cookieParser from "cookie-parser"
@@ -14,7 +21,7 @@ import { app ,server} from "./socket/socket.js"
 dotenv.config()
 
 const port = process.env.PORT || 5000;
-const __dirname = path.resolve();
+ 
 
 app.use(express.json())  // to parse the incoming requests with JSON payloads  (from req.body)
 app.use(cookieParser())
@@ -26,11 +33,11 @@ app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "fronted", "dist",  "index.html"));
-});
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "fronted", "dist",  "index.html"));
+// });
 
 
 
